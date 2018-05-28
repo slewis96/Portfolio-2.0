a404dd493986dc5125957bf00de26df2e881e7f4
slewis96 <seblewis96@gmail.com>
2018-05-28 12:03:07 +0100

    Added contact section and button highlights

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Portfolio-2.0
 ## TODO:
-- [ ] **Scroll snap**
-- [ ] **Set active button based on viewpoint**
+- [ ] Fix links to work one after another
+- [x] **Scroll snap**
+- [x] **Set active button based on viewpoint**
+- [x] Add contact page
 - [x] Add Company and University images
 - [x] Fix background
 - [x] Ensure no overflow on text (check padding) (check bootstrap)
@@ -12,3 +14,4 @@
 
 ## Notes
 https://css-tricks.com/introducing-css-scroll-snap-points/
+https://projects.lukehaas.me/scrollify/#home
--- js/main.js
+++ js/main.js
@@ -2,19 +2,29 @@ $(function() {
 	$.scrollify({
 		section:".overlay",
 		scrollSpeed:1000,
-		after:function(i) {
+		before:function(i) {
+			switch($.scrollify.currentIndex()){
+				case 0:
+					$("button").removeClass("active");
+					$("#btnHome").addClass("active");
+					break;
+				case 1:
+					$("button").removeClass("active");
+					$("#btnEmployment").addClass("active");
+					break;
+				case 2:
+					$("button").removeClass("active");
+					$("#btnEducation").addClass("active");
+					break;
+				case 3:
+					$("button").removeClass("active");
+					$("#btnSkills").addClass("active");
+					break;
+				case 4:
+					$("button").removeClass("active");
+					$("#btnContact").addClass("active");
+					break;
+			}
 		}
-	});
-	$(".scroll,.scroll-btn").click(function(e) {
-		e.preventDefault();
-		$.scrollify.next();
 	});
 });
-function nextScroll(x){
-  $.scrollify({
-		section:x,
-		scrollSpeed:1800,
-		after:function(i) {
-		}
-	});
-}
